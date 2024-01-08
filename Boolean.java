9f64a12166cb8632416a0f2d0c76ac5f1ae95fbd
emisemisRoxo <156017135+emisemisRoxo@users.noreply.github.com>
2024-01-08 16:46:01 -0300

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,6 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean VouF = true;
+        System.out.println(VouF);
+    }
+}
---
+++ Teste.java
@@ -0,0 +1,8 @@
+/**
+ * Teste
+ */
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
