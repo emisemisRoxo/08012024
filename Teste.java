a531414bb62df0fae5018743563dc751db8dbcfe
emisemisRoxo <156017135+emisemisRoxo@users.noreply.github.com>
2024-01-08 16:54:03 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -3,6 +3,10 @@
  */
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(ImprimirTextoMaiusculo("Hello World " + (1 + 6 * 7)));
+    }
+    public static String ImprimirTextoMaiusculo(String Texto)
+    {
+        return Texto.toUpperCase();
     }
 }
