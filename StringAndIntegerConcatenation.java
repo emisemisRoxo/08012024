a4aa00b53f8619fed54efb96b613b3e68ad9bc4d
emisemisRoxo <156017135+emisemisRoxo@users.noreply.github.com>
2024-01-09 17:20:22 -0300

    Delete StringAndIntegerConcatenation.java

--- StringAndIntegerConcatenation.java
+++
@@ -1,7 +0,0 @@
-public class StringAndIntegerConcatenation {
-    public static void main(String[] args) {
-        int quant = 50;
-        String prof = "alunos";
-        System.out.println("No senac temos " + quant + " " + prof + ".");
-    }
-}
